220825ff7e2506e65f3ab994944bd6135855817d
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-10-26 23:01:53 +0100

    Thu Oct 26 23:01:53 BST 2017

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,7 +1,8 @@
 # noinspection PyUnusedLocal
 import string
-PRICE_LIST = [50, 30, 20, 15, 40, 10, 20, 10, 35, 60, 80, E
-              PRICE_LIST = dict(zip(string.ascii_uppercase, PRICE_LIST))
+
+PRICE_LIST = [50, 30, 20, 15, 40, 10, 20, 10, 35, 60, 80, 90, 15, 40, 10, 50, 30, 50, 30, 20, 40, 50, 20, 90, 10, 50]
+PRICE_LIST = dict(zip(string.ascii_uppercase, PRICE_LIST))
 DISCOUNTS = {
     'A': {5: 200, 3: 130},
     'B': {2: 45},
@@ -12,13 +13,14 @@ DISCOUNTS = {
     'V': {3: 130, 2: 90}
 }
 
-FREEBIES ={
+FREEBIES = {
     'E': {2: 'B'},
     'F': {2: 'F'},
     'N': {3: 'M'},
     'R': {3: 'Q'},
     'U': {3: 'U'}
 }
+
 
 def price_freebies(counts):
     price = 0
@@ -33,6 +35,7 @@ def price_freebies(counts):
                 counts[target_sku] = (counts[target_sku] - 1) if counts[target_sku] > 0 else counts[target_sku]
     return price
 
+
 def price_discounts(counts):
     price = 0
     for sku_disc in DISCOUNTS.keys():
@@ -42,6 +45,7 @@ def price_discounts(counts):
             price += (counts[sku_disc] // item_count) * dis
             counts[sku_disc] = counts[sku_disc] % item_count
     return price
+
 
 def price_all(counts):
     price = 0
