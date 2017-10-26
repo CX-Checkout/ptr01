71bd6b4515c99c3c0f92c8558ddc455dcc83c0f1
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-10-26 22:30:29 +0100

    Thu Oct 26 22:30:29 BST 2017

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,3 +1,13 @@
 # noinspection PyUnusedLocal
 def checkout(skus):
-    raise NotImplementedError()
+    if len(skus) == 0:
+        return -1
+    counts = {}
+    for item in ['A', 'B', 'C', 'D']:
+        counts[item] = 0
+        for item in skus:
+            counts[item.upper()] += 1
+    return ((counts['D'] * 15) +
+            (counts['C'] * 20) +
+            ((counts['B'] // 2) * 45 + (counts['B'] % 2) * 30) +
+            ((counts['A'] // 3) * 130 + (counts['A'] % 3) * 50))
