0c526ec621713e0120544b2edc3aede2e8fcd8b7
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-10-26 22:44:13 +0100

    Thu Oct 26 22:44:13 BST 2017

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,19 +1,12 @@
 # noinspection PyUnusedLocal
-def checkout(skus):
-    if len(skus) == 0:
-        return 0
-    counts = {}
-    for item in ['A', 'B', 'C', 'D']:
-        counts[item] = 0
-    for item in skus:
-        if item not in ['A', 'B', 'C', 'D']:
-            return -1
-        counts[item.upper()] += 1
-    return ((counts['D'] * 15) +
-            (counts['C'] * 20) +
-            ((counts['B'] // 2) * 45 + (counts['B'] % 2) * 30) +
-            ((counts['A'] // 3) * 130 + (counts['A'] % 3) * 50))
-
+def price_a(a):
+    price = 0
+    price += (a // 5) * 200
+    a = a % 4
+    price += (a // 3) * 130
+    a = a % 3
+    price += a * 50
+    return price
 
 def checkout(skus):
     if len(skus) == 0:
@@ -33,5 +26,5 @@ def checkout(skus):
         (counts['D'] * 15) +
         (counts['C'] * 20) +
         ((counts['B'] // 2) * 45 + (counts['B'] % 2) * 30) +
-    price_a(counts[‘A'])
+        price_a(counts['A'])
     )
