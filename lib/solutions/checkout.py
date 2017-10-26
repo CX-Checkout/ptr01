960952be91080e03da1f8e12d383aae97e1f3244
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-10-26 22:32:25 +0100

    Thu Oct 26 22:32:25 BST 2017

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -6,7 +6,8 @@ def checkout(skus):
     for item in ['A', 'B', 'C', 'D']:
         counts[item] = 0
     for item in skus:
-        counts[item.upper()] += 1
+        if item.upper() in ['A', 'B', 'C', 'D']:
+            counts[item.upper()] += 1
     return ((counts['D'] * 15) +
             (counts['C'] * 20) +
             ((counts['B'] // 2) * 45 + (counts['B'] % 2) * 30) +
