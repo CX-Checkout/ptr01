b2ec0cb77bc218b6ca05e015d932d89d4033cc89
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-10-26 22:51:36 +0100

    Thu Oct 26 22:51:36 BST 2017

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -9,24 +9,26 @@ def price_a(a):
     return price
 
 
+def price_all(counts):
+    price = 0
+    for sku in counts.keys():
+        price += counts[sku] * PRICE_LIST[sku]
+    return price
+
+
 def checkout(skus):
     if len(skus) == 0:
         return 0
     counts = {}
-    item_names = ['A', 'B', 'C', 'D', 'E', 'F']
+    item_names = PRICE_LIST.keys()
     for item in item_names:
         counts[item] = 0
     for item in skus:
         if item not in item_names:
             return -1
         counts[item] += 1
-    counts['B'] = counts['B'] - (counts['E'] // 2)
-    counts['B'] = 0 if counts['B'] < 0 else counts['B']
-    return (
-        ((counts['F'] // 3) * 20 + ((counts['F'] % 3)*10)) +
-        (counts['E'] * 40) +
-        (counts['D'] * 15) +
-        (counts['C'] * 20) +
-        ((counts['B'] // 2) * 45 + (counts['B'] % 2) * 30) +
-        price_a(counts['A'])
-    )
+    price = 0
+    price += (price_freebies(counts) +
+              price_discounts(counts) +
+              price_all(counts))
+    return price
