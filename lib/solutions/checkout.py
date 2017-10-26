be8709b59e4a1be0ecb3cce63556054d246a08bc
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-10-26 22:42:32 +0100

    Thu Oct 26 22:42:32 BST 2017

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -13,3 +13,25 @@ def checkout(skus):
             (counts['C'] * 20) +
             ((counts['B'] // 2) * 45 + (counts['B'] % 2) * 30) +
             ((counts['A'] // 3) * 130 + (counts['A'] % 3) * 50))
+
+
+def checkout(skus):
+    if len(skus) == 0:
+        return 0
+    counts = {}
+    item_names = ['A', 'B', 'C', 'D', 'E']
+    for item in item_names:
+        counts[item] = 0
+    for item in skus:
+        if item not in item_names:
+            return -1
+        counts[item] += 1
+    counts['B'] = counts['B'] - (counts['E'] // 2)
+    counts['B'] = 0 if counts['B'] < 0 else counts['B']
+    return (
+        (counts['E'] * 40) +
+        (counts['D'] * 15) +
+        (counts['C'] * 20) +
+        ((counts['B'] // 2) * 45 + (counts['B'] % 2) * 30) +
+    price_a(counts[‘A'])
+    )
